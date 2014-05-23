e77a080915b39cc08f14064eef72e5f9c1d4ad88
Administrator <admin@local.host>
2014-05-23 15:39:42 +0800

    Edits test.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -6,42 +6,40 @@ describe ProjectsController do
   let(:png)     { fixture_file_upload(Rails.root + 'spec/fixtures/dk.png', 'image/png') }
   let(:jpg)     { fixture_file_upload(Rails.root + 'spec/fixtures/rails_sample.jpg', 'image/jpg') }
   let(:gif)     { fixture_file_upload(Rails.root + 'spec/fixtures/banana_sample.gif', 'image/gif') }
-  let(:gif)     { fixture_file_upload(Rails.root + 'spec/fixtures/doc_sample.txt', 'text/plain') }
+  let(:txt)     { fixture_file_upload(Rails.root + 'spec/fixtures/doc_sample.txt', 'text/plain') }
 
   describe "POST #upload_image" do
     before do
       sign_in(user)
+      binding.pry
     end
 
     context "without params['markdown_img']" do
       it "returns an error" do
-        post :upload_image, project_id: project.to_param
-        expect(response.status).to eq(400) #bad request
+        post :upload_image, id: project.to_param
+        expect(response.status).to eq(404)
       end
     end
 
     context "with invalid file" do
+      before do
+        post :upload_image, id: project.to_param, markdown_img: @img
+        binding.pry
+      end
+
       it "returns an error" do
-        expect(response.status).to eq(400) #bad request
+        expect(response.status).to eq(404)
       end
     end
 
     context "with valid file" do
       before do
-        post :upload_image, project_id: project.to_param, markdown_img: jpg
+        post :upload_image, id: project.to_param, markdown_img: @img
       end
 
-      it "returns a JSON object" do
-        expect(response.headers['Content-Type']).to have_content 'application/json'
-      end
-
-      it "returns a content with original filename and link." do
-        link = {alt: 'rails_sample', link: }
-        expect(response.body).to 
-      end
-
-      it "returns a correct" do
-        
+      it "returns a content with original filename and new link." do
+        link = { alt: 'rails_sample', link: '' }.to_json
+        expect(response.body).to have_content link
       end
     end
   end
